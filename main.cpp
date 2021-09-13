8b68dcc750132c58dd11f1e6567d17f90e1e1629
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2021-09-13 12:03:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
+#include "vulkan/vulkan.hpp"
+#include "vulkan/vulkan_raii.hpp"
+VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
+
+#define VMA_IMPLEMENTATION
+#include "vk_mem_alloc.h"
+
+#define GLFW_INCLUDE_NONE
+#include "GLFW/glfw3.h"
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_vulkan.h"
+
+#include "glm/glm.hpp"
+using namespace glm;
+
 int main(int, char**) {
   std::cout << "cpp-2021-vulkan\n";
 }
