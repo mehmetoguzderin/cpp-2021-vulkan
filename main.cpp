730ea2a24b77d2fe92fc2d1adaf58643aee3554c
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2021-09-14 13:40:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /* Inspired by
- * https://github.com/KhronosGroup/Vulkan-Hpp/tree/master/samples
+ * https://github.com/KhronosGroup/Vulkan-Hpp/tree/master/RAII_Samples
  * https://github.com/KhronosGroup/Vulkan-Tools/tree/master/cube
  * https://github.com/KhronosGroup/Vulkan-Samples/tree/master/samples/extensions/raytracing_basic
  * https://github.com/glfw/glfw/blob/master/tests/triangle-vulkan.c
@@ -116,6 +116,62 @@ struct Main {
       vk::DeviceQueueCreateInfo deviceQueueCreateInfo({}, static_cast<uint32_t>(queueFamilyIndex), 1, &queuePriority);
       vk::DeviceCreateInfo deviceCreateInfo({}, deviceQueueCreateInfo);
       vk::raii::Device device(physicalDevice, deviceCreateInfo);
+      VmaVulkanFunctions allocatorVulkanFunctions{};
+#define VMA_VULKAN_FUNCTIONS_RAII_INSTANCE(functionName) \
+  allocatorVulkanFunctions.functionName = instance.getDispatcher()->functionName
+#define VMA_VULKAN_FUNCTIONS_RAII_DEVICE(functionName) \
+  allocatorVulkanFunctions.functionName = device.getDispatcher()->functionName;
+#define VMA_VULKAN_KHR_FUNCTIONS_RAII_INSTANCE(functionName)                             \
+  if (instance.getDispatcher()->functionName##KHR == nullptr)                            \
+    allocatorVulkanFunctions.functionName##KHR = instance.getDispatcher()->functionName; \
+  else                                                                                   \
+    allocatorVulkanFunctions.functionName##KHR = instance.getDispatcher()->functionName##KHR;
+#define VMA_VULKAN_KHR_FUNCTIONS_RAII_DEVICE(functionName)                             \
+  if (device.getDispatcher()->functionName##KHR == nullptr)                            \
+    allocatorVulkanFunctions.functionName##KHR = device.getDispatcher()->functionName; \
+  else                                                                                 \
+    allocatorVulkanFunctions.functionName##KHR = device.getDispatcher()->functionName##KHR;
+      VMA_VULKAN_FUNCTIONS_RAII_INSTANCE(vkGetPhysicalDeviceProperties);
+      VMA_VULKAN_FUNCTIONS_RAII_INSTANCE(vkGetPhysicalDeviceMemoryProperties);
+      VMA_VULKAN_FUNCTIONS_RAII_DEVICE(vkAllocateMemory);
+      VMA_VULKAN_FUNCTIONS_RAII_DEVICE(vkFreeMemory);
+      VMA_VULKAN_FUNCTIONS_RAII_DEVICE(vkMapMemory);
+      VMA_VULKAN_FUNCTIONS_RAII_DEVICE(vkUnmapMemory);
+      VMA_VULKAN_FUNCTIONS_RAII_DEVICE(vkFlushMappedMemoryRanges);
+      VMA_VULKAN_FUNCTIONS_RAII_DEVICE(vkInvalidateMappedMemoryRanges);
+      VMA_VULKAN_FUNCTIONS_RAII_DEVICE(vkBindBufferMemory);
+      VMA_VULKAN_FUNCTIONS_RAII_DEVICE(vkBindImageMemory);
+      VMA_VULKAN_FUNCTIONS_RAII_DEVICE(vkGetBufferMemoryRequirements);
+      VMA_VULKAN_FUNCTIONS_RAII_DEVICE(vkGetImageMemoryRequirements);
+      VMA_VULKAN_FUNCTIONS_RAII_DEVICE(vkCreateBuffer);
+      VMA_VULKAN_FUNCTIONS_RAII_DEVICE(vkDestroyBuffer);
+      VMA_VULKAN_FUNCTIONS_RAII_DEVICE(vkCreateImage);
+      VMA_VULKAN_FUNCTIONS_RAII_DEVICE(vkDestroyImage);
+      VMA_VULKAN_FUNCTIONS_RAII_DEVICE(vkCmdCopyBuffer);
+      VMA_VULKAN_KHR_FUNCTIONS_RAII_DEVICE(vkGetBufferMemoryRequirements2);
+      VMA_VULKAN_KHR_FUNCTIONS_RAII_DEVICE(vkGetImageMemoryRequirements2);
+      VMA_VULKAN_KHR_FUNCTIONS_RAII_DEVICE(vkBindBufferMemory2);
+      VMA_VULKAN_KHR_FUNCTIONS_RAII_DEVICE(vkBindImageMemory2);
+      VMA_VULKAN_KHR_FUNCTIONS_RAII_INSTANCE(vkGetPhysicalDeviceMemoryProperties2);
+#undef VMA_VULKAN_KHR_FUNCTIONS_RAII_DEVICE
+#undef VMA_VULKAN_KHR_FUNCTIONS_RAII_INSTANCE
+#undef VMA_VULKAN_FUNCTIONS_RAII_DEVICE
+#undef VMA_VULKAN_FUNCTIONS_RAII_INSTANCE
+      VmaAllocatorCreateInfo allocatorCreateInfo{
+          .flags = 0,
+          .physicalDevice = static_cast<VkPhysicalDevice>(*physicalDevice),
+          .device = static_cast<VkDevice>(*device),
+          .pVulkanFunctions = &allocatorVulkanFunctions,
+          .instance = static_cast<VkInstance>(*instance),
+          .vulkanApiVersion = VK_API_VERSION_1_2,
+      };
+      VmaAllocator allocator;
+      if (vmaCreateAllocator(&allocatorCreateInfo, &allocator) != VK_SUCCESS) {
+        throw std::runtime_error("vmaCreateAllocator(&allocatorCreateInfo, &allocator) != VK_SUCCESS");
+      }
+      {  // Allocator
+      }
+      vmaDestroyAllocator(allocator);
       std::cout << "cpp-2021-vulkan\n";  // main
     } catch (vk::SystemError& err) {
       std::cout << "vk::SystemError: " << err.what() << "\n";
